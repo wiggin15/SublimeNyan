0784018c4cbb743e2c35f99bae33adbcb52932f0
Arnon Yaari <arnony@infinidat.com>
2014-08-11 13:03:47 +0300

    refactor plugin

--- nyan.py
+++ nyan.py
@@ -1,26 +1,18 @@
 import sublime, sublime_plugin
 
-"""
-0x1351 nyan-frame-1
-0x1354 nyan-frame-2
-0x1363 nyan-frame-3
-0x1365 nyan-frame-4
-0x1368 nyan-frame-5
-0x1369 nyan-frame-6
-0x136b rainbow
-0x1375 outerspace
-"""
+FRAMES = u"\u1351\u1354\u1363\u1365\u1368\u1369"
+RAINBOW = u'\u136b'
+OUTERSPACE = u"\u1375"
+NYAN_SIZE = 40
 
 class NyanListener(sublime_plugin.EventListener):
     def __init__(self):
+        from itertools import cycle
         super(NyanListener, self).__init__()
-        self.frame = 0
+        self.frames = cycle(FRAMES)
 
     def on_selection_modified(self, view):
         pos = float(view.sel()[-1].end()) / float(view.size())
-        nyan_size = 40
-        num_bullets = min(nyan_size -1, int(nyan_size * pos))
-        frames = u"\u1351\u1354\u1363\u1365\u1368\u1369"
-        s = u'\u136b' * num_bullets + frames[self.frame] + u"\u1375" * max(0, (nyan_size - num_bullets - 1))
-        self.frame = (self.frame + 1) % len(frames)
+        num_bullets = min(NYAN_SIZE - 1, int(NYAN_SIZE * pos))
+        s = RAINBOW * num_bullets + next(self.frames) + OUTERSPACE * max(0, (NYAN_SIZE - num_bullets - 1))
         view.set_status('anyan', s)
